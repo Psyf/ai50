0ed99ea93e6244cb00e8ce5972ce83e7ba80b5cf
Psyf <saifum@u.nus.edu>
2020-05-17 01:48:56 +0800

    wrote the some utility functions

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -6,7 +6,8 @@ import math
 
 X = "X"
 O = "O"
-EMPTY = None
+EMPTY = ""
+NUM_MATCH = 3
 
 
 def initial_state():
@@ -22,7 +23,21 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+
+    x_count = 0
+    o_count = 0
+
+    for row in board: 
+        for mark in row: 
+            if mark == X: 
+                x_count += 1
+            elif mark == O: 
+                o_count += 1
+
+    if (x_count+o_count)%2: 
+        return O 
+    else: 
+        return X
 
 
 def actions(board):
@@ -43,21 +58,82 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    # check horizontal wins
+    result = ""
+    for row in board: 
+        for item in row: 
+            result += item
+
+        if X*NUM_MATCH in result: 
+            return X
+        elif O*NUM_MATCH in result: 
+            return O
+        result = ""
+
+    # check vertical
+    result = ""
+    for col in range(len(board[0])): 
+        for row in range(len(board)): 
+            result += board[row][col]
+
+        if X*NUM_MATCH in result: 
+            return X
+        elif O*NUM_MATCH in result: 
+            return O
+        result = ""
+
+
+    # check diagonal \
+    result = ""
+    for i in range(len(board)):
+        result += board[i][i]
+        
+    if X*NUM_MATCH in result: 
+        return X
+    elif O*NUM_MATCH in result: 
+        return O
+    result = ""
+
+    # check diagonal /
+    result = ""
+    for i in range(len(board)-1, -1, -1):
+        result += board[NUM_MATCH-i-1][i]
+        
+    if X*NUM_MATCH in result: 
+        return X
+    elif O*NUM_MATCH in result: 
+        return O
+    result = ""
+
+    return None
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    
+    if winner(board) is not None: 
+        return True
+    else: 
+        for row in board: 
+            for item in row: 
+                if item is EMPTY: 
+                    return False 
+        return True
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    winner = winner(board)
+    if winner == X: 
+        return 1
+    elif winner == O: 
+        return -1 
+    else: 
+        return 0
 
 
 def minimax(board):
@@ -65,3 +141,7 @@ def minimax(board):
     Returns the optimal action for the current player on the board.
     """
     raise NotImplementedError
+
+print(terminal([[X, EMPTY, X],
+            [O, EMPTY, X],
+            [X, EMPTY, O]]))
